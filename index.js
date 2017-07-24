53465aabaecee87b2da57fa49664d8dcac5a33f4
Zach Lowe <lowezake@gmail.com>
2017-07-24 16:20:02 -0600

    push part2

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 (function() {
+
+
   'use strict';
   const stop = document.getElementById("stopButton")
   const stop1 = document.getElementById("stopLight")
   stop.addEventListener("click", function () {
         stop1.classList.toggle("stop")
+        });
+  stop.addEventListener("mouseenter", function () {
+          console.log("Entered stop button")
+        });
+  stop.addEventListener("mouseleave", function () {
+          console.log("Left stop button")
         });
 
   const slow = document.getElementById("slowButton")
@@ -11,12 +19,27 @@
   slow.addEventListener("click", function () {
         slow1.classList.toggle("slow")
         });
+  slow.addEventListener("mouseenter", function () {
+        console.log("Entered slow button")
+        });
+  slow.addEventListener("mouseleave", function () {
+          console.log("Left slow button")
+        });
 
   const go = document.getElementById("goButton")
   const go1 = document.getElementById("goLight")
   go.addEventListener("click", function () {
         go1.classList.toggle("go")
         });
+  go.addEventListener("mouseenter", function () {
+          console.log("Entered go button")
+        });
+  go.addEventListener("mouseleave", function () {
+          console.log("Left go button")
+        });
+
+
+
 })();
 
 /*
